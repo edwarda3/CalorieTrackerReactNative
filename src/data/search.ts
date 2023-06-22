1ad4e624861bcbb8e7212f766a0e2bafdde8e569
Alex Edwards <alexedwards31@gmail.com>
2023-06-21 20:19:57 -0700

    Limit search to 100 via pagination, add percentage of total stat

---
+++ src/data/search.ts
@@ -0,0 +1,77 @@
+import _ from "lodash";
+import { getDateString } from "../types/Dates";
+import { Database, MealData } from "../types/Model";
+
+export interface DaySearchResult {
+    dateString: string;
+    dayResult: MealData[];
+    matchedItemTotalKcal: number;
+    daySearchTotalKcal: number;
+};
+
+export const MINIMUM_NAME_SEARCH_LENGTH = 3;
+
+export const filterNameByRegexIfValid = (regexFilter: string, kcalFilter: number, mealDataList: MealData[]): MealData[] => {
+    let regex: RegExp | null;
+    try {
+        regex = new RegExp(regexFilter.replace('*', '.*').trim(), 'i');
+    } catch (err) {
+        regex = null
+    }
+    return mealDataList.filter((mealData) => {
+        const nameMatches = !!regex ? regex.test(mealData.name) : mealData.name.toLowerCase().includes(regexFilter.toLowerCase());
+        const caloriesMeetMinumum = (mealData.kcalPerServing * mealData.servings) >= Number(kcalFilter);
+        return nameMatches && caloriesMeetMinumum;
+    });
+}
+
+export interface SearchForMealsOptions {
+    nameFilter: string;
+    minimumKcalFilter?: number;
+    minDate?: Date;
+    maxDate?: Date;
+    startFromDateString?: string | null;
+    maxResults?: number;
+}
+
+export interface SearchForMealOutput {
+    searchResult: Array<DaySearchResult>;
+    searchFoundCount: number;
+    cursor: string|null;
+}
+
+export const searchForMeals = (database: Database, options: SearchForMealsOptions): SearchForMealOutput => {
+    // key of map will be date in YYYY-MM-DD form.
+    const searchResult: Array<DaySearchResult> = [];
+    let totalEntryCount = 0;
+    for (const ymKey of Object.keys(database).sort().reverse()) {
+        for (const dayKey of Object.keys(database[ymKey]).sort().reverse()) {
+            const dateString = `${ymKey}-${dayKey}`;
+            if (
+                (options.startFromDateString && dateString >= options.startFromDateString) ||
+                (options.minDate && dateString < getDateString(options.minDate)) ||
+                (options.maxDate && dateString > getDateString(options.maxDate))
+            ) {
+                continue;
+                // pass
+            } else {
+                const matchedEntriesInThatDay = filterNameByRegexIfValid(options.nameFilter, options.minimumKcalFilter ?? 0, database[ymKey][dayKey]);
+                if (!_.isEmpty(matchedEntriesInThatDay)) {
+                    const totalSum: number = database[ymKey][dayKey].reduce((acc, current) => acc + (current.kcalPerServing * current.servings), 0);
+                    const matchedSum = matchedEntriesInThatDay.reduce((acc, current) => acc + (current.kcalPerServing * current.servings), 0)
+                    searchResult.push({
+                        dateString,
+                        dayResult: matchedEntriesInThatDay,
+                        matchedItemTotalKcal: matchedSum,
+                        daySearchTotalKcal: totalSum,
+                    });
+                }
+                totalEntryCount += matchedEntriesInThatDay.length;
+                if (options.maxResults && totalEntryCount >= options.maxResults) { 
+                    return { searchResult, searchFoundCount: totalEntryCount, cursor: dateString };
+                }
+            }
+        }
+    }
+    return { searchResult, searchFoundCount: totalEntryCount, cursor: null };
+}
--- src/pages/SearchByMeal.tsx
+++ src/pages/SearchByMeal.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { ActivityIndicator, FlatList, Keyboard, Modal, Text, TextInput, View } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { ActivityIndicator, Button, FlatList, Keyboard, Modal, Text, TextInput, View } from 'react-native';
 import { bespokeStyle, styles } from '../styles/Styles';
 import { DataStore, Database, MealData } from '../types/Model';
 import _ from 'lodash';
@@ -15,31 +15,10 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { ExtensibleButton } from '../components/Buttons';
 import Collapsible from 'react-native-collapsible';
 import DateTimePicker from '@react-native-community/datetimepicker';
-import { getDateString, getDateStringParts, getYearMonthIndex } from '../types/Dates';
+import { formatDate, getDateString, getDateStringParts, getYearMonthIndex } from '../types/Dates';
 import Toast from 'react-native-toast-message';
 import { ScrollView } from 'native-base';
-
-interface DaySearchResult {
-    dateString: string;
-    dayResult: MealData[];
-    daySearchTotalKcal: number;
-};
-
-const minimumNameSearchLength = 3;
-
-const filterNameByRegexIfValid = (regexFilter: string, kcalFilter: number, mealDataList: MealData[]): MealData[] => {
-    let regex: RegExp | null;
-    try {
-        regex = new RegExp(regexFilter.replace('*', '.*').trim(), 'i');
-    } catch (err) {
-        regex = null
-    }
-    return mealDataList.filter((mealData) => {
-        const nameMatches = !!regex ? regex.test(mealData.name) : mealData.name.toLowerCase().includes(regexFilter.toLowerCase());
-        const caloriesMeetMinumum = (mealData.kcalPerServing * mealData.servings) >= Number(kcalFilter);
-        return nameMatches && caloriesMeetMinumum;
-    });
-}
+import { DaySearchResult, SearchForMealsOptions, searchForMeals } from '../data/search';
 
 export const SearchByMeal = (props: NavigatedScreenProps) => {
     const [dataStore, setDatastore] = useState<DataStore | null>(null);
@@ -50,6 +29,9 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
     monthAgo.setDate(monthAgo.getDate() - 30);
     const [minDate, setMinDate] = useState<Date>(monthAgo); // TODO make this 1 month ago (use dayjs?)
     const [maxDate, setMaxDate] = useState<Date>(new Date());
+    const [dateStringCursor, setDateStringCursor] = useState<string | null>(null);
+    const [foundResult, setFoundResult] = useState<Array<DaySearchResult>>([]);
+    const [totalFound, setTotalFound] = useState<number>(0);
 
     const refresh = async () => {
         const dataStore = await DatabaseHandler.getInstance().getAllKnownData();
@@ -63,45 +45,61 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
         }, [])
     );
 
-    const updateFilter = ({ name, kcals }: { name?: string; kcals?: string }) => {
-        if (!_.isNil(name)) setNameFilter(name);
-        if (kcals === '' || !isNaN(Number(kcals))) setTotalKcalFilterStr(kcals ?? '');
-    }
-
-    // key of map will be date in YYYY-MM-DD form.
-    const searchResult: Array<DaySearchResult> = [];
-    if (dataStore && nameFilter.length >= minimumNameSearchLength) {
-        Object.keys(dataStore.database ?? {}).sort().reverse().forEach((ymKey) => {
-            Object.keys(dataStore.database[ymKey]).sort().reverse().forEach((dayKey) => {
-                const dateString = `${ymKey}-${dayKey}`;
-                if (showDateFilters && (dateString < getDateString(minDate)) || dateString > getDateString(maxDate)) {
-                    return;
-                }
-                const minimumKcalFilter = isNaN(Number(totalKcalFilterStr)) ? 0 : Number(totalKcalFilterStr);
-                const matchedEntriesInThatDay = filterNameByRegexIfValid(nameFilter, minimumKcalFilter, dataStore.database[ymKey][dayKey]);
-                if (!_.isEmpty(matchedEntriesInThatDay)) {
-                    const totalSum: number = dataStore.database[ymKey][dayKey].reduce((acc, current) => acc + (current.kcalPerServing * current.servings), 0);
-                    searchResult.push({
-                        dateString,
-                        dayResult: matchedEntriesInThatDay,
-                        daySearchTotalKcal: totalSum,
-                    });
-                }
+    const performSearch = (options?: Partial<SearchForMealsOptions>, replace: boolean = false) => {
+        if (dataStore?.database) {
+            const searchOptionsByState: SearchForMealsOptions = {
+                nameFilter: nameFilter,
+                minimumKcalFilter: isNaN(Number(totalKcalFilterStr)) ? 0 : Number(totalKcalFilterStr),
+                startFromDateString: replace ? null : dateStringCursor,
+                maxResults: 100,
+            };
+            if (showDateFilters) {
+                searchOptionsByState.minDate = minDate;
+                searchOptionsByState.maxDate = maxDate;
+            }
+            const { searchResult, searchFoundCount, cursor } = searchForMeals(dataStore.database, {
+                ...searchOptionsByState,
+                ...(options ?? {})
             });
-        });
+            setFoundResult(replace ? searchResult : [...foundResult, ...searchResult]);
+            setTotalFound(replace ? searchFoundCount : totalFound + searchFoundCount)
+            setDateStringCursor(cursor);
+        }
     }
 
-    const getDaySearchResult = (daySearchResult: DaySearchResult) => (
-        <ContextMenu
+    const updateFilter = ({ name, kcals }: { name?: string; kcals?: string }) => {
+        if (_.isEmpty(name) && _.isNil(kcals)) {
+            setNameFilter('');
+            setFoundResult([]);
+            setTotalFound(0)
+            setDateStringCursor(null);
+        } else {
+            const toUpdate: Partial<SearchForMealsOptions> = {};
+            if (!_.isNil(name)) {
+                toUpdate.nameFilter = name;
+                setNameFilter(name);
+            }
+            if (kcals === '' || !isNaN(Number(kcals))) {
+                toUpdate.minimumKcalFilter = isNaN(Number(kcals)) ? 0 : Number(kcals);
+                setTotalKcalFilterStr(kcals ?? '')
+            }
+            setDateStringCursor(null);
+            performSearch(toUpdate, true);
+        }
+    }
+
+    const getDaySearchResultDisplay = (daySearchResult: DaySearchResult) => {
+        const { dayResult, dateString, matchedItemTotalKcal, daySearchTotalKcal } = daySearchResult;
+        return <ContextMenu
             previewBackgroundColor='rgba(0,0,0,0)'
-            key={daySearchResult.dateString}
+            key={dateString}
             actions={[
                 { title: 'Go to Day' }
             ]}
             onPress={({ nativeEvent }) => {
                 if (nativeEvent.name === 'Go to Day') {
                     const dayPageParams: DayPageParams = {
-                        dateString: daySearchResult.dateString,
+                        dateString: dateString,
                     }
                     props.navigation.navigate(NavigationPages.DAY, dayPageParams)
                 }
@@ -109,16 +107,16 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
         >
             <View style={{ padding: 10 }}>
                 <View style={{ flexDirection: 'row', gap: 10 }}>
-                    <Text style={bespokeStyle('subLabel', { flexGrow: 1 })}>{daySearchResult.dateString}</Text>
-                    <Text style={styles.subLabel}>Day total: {daySearchResult.daySearchTotalKcal}kcal</Text>
+                    <Text style={bespokeStyle('subLabel', { flexGrow: 1 })}>{formatDate(new Date(dateString))}</Text>
+                    <Text style={styles.subLabel}>{matchedItemTotalKcal} of {daySearchTotalKcal}kcal ({Math.round(100 * (matchedItemTotalKcal / daySearchTotalKcal))}% of day)</Text>
                 </View>
-                {_.map(daySearchResult.dayResult, (mealData) => {
+                {_.map(dayResult, (mealData) => {
                     const existingPreset = dataStore?.presets.find((preset) => preset.name === mealData.name);
-                    return <MealEntryListItem key={`${mealData.name}-${mealData.time}-${daySearchResult.dateString}`} meal={mealData} actions={[
+                    return <MealEntryListItem key={`${mealData.name}-${mealData.time}-${dateString}`} meal={mealData} actions={[
                         {
                             title: 'Edit Entry', onPress: () => {
                                 navigateToItemPage(dataStore?.settings!, props.navigation, {
-                                    dateString: daySearchResult.dateString,
+                                    dateString: dateString,
                                     itemName: mealData.name,
                                     itemTime: mealData.time,
                                 })
@@ -156,7 +154,7 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
                             title: 'Go to Day',
                             onPress: () => {
                                 const dayPageParams: DayPageParams = {
-                                    dateString: daySearchResult.dateString,
+                                    dateString: dateString,
                                 }
                                 props.navigation.navigate(NavigationPages.DAY, dayPageParams)
                             }
@@ -166,7 +164,7 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
             </View>
             <HorizontalLine />
         </ContextMenu>
-    )
+    }
 
     return (
         <SafeAreaView style={{
@@ -188,14 +186,20 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
                     placeholderTextColor='grey'
                     inputMode='numeric'
                 />
-                <ExtensibleButton title='Filters' style={bespokeStyle('subLabel', {padding: 20})} onPress={() => setShowDateFilters(!showDateFilters)} />
+                <ExtensibleButton title='Filters' style={bespokeStyle('subLabel', { padding: 20 })} onPress={() => {
+                    setShowDateFilters(!showDateFilters);
+                    setDateStringCursor(null);
+                    performSearch(showDateFilters ? { minDate, maxDate } : {}, true);
+                }} />
             </View>
-            <Collapsible collapsed={!showDateFilters} style={{flexDirection: 'row', alignItems: 'center', gap: 10, justifyContent: 'center'}}>
+            <Collapsible collapsed={!showDateFilters} style={{ flexDirection: 'row', alignItems: 'center', gap: 10, justifyContent: 'center' }}>
                 <Text style={styles.subLabel}>From</Text>
                 <DateTimePicker
                     onChange={({ type }, date) => {
                         if (type === 'set' && date) {
-                            setMinDate(date)
+                            setMinDate(date);
+                            setDateStringCursor(null);
+                            performSearch({ minDate: date }, true);
                         }
                     }}
                     value={minDate}
@@ -206,7 +210,9 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
                 <DateTimePicker
                     onChange={({ type }, date) => {
                         if (type === 'set' && date) {
-                            setMaxDate(date)
+                            setMaxDate(date);
+                            setDateStringCursor(null);
+                            performSearch({ maxDate: date }, true);
                         }
                     }}
                     value={maxDate}
@@ -214,10 +220,11 @@ export const SearchByMeal = (props: NavigatedScreenProps) => {
                     locale='en'
                 />
             </Collapsible>
-            <Text style={bespokeStyle('subLabel', { paddingHorizontal: 10, marginTop: 10 })}>{searchResult.length} results found</Text>
+            <Text style={bespokeStyle('subLabel', { paddingHorizontal: 10, marginTop: 10 })}>{totalFound}{dateStringCursor ? '+' : ''} results found</Text>
             <HorizontalLine marginTop={10} />
             <ScrollView indicatorStyle='black' onScrollBeginDrag={() => Keyboard.dismiss()}>
-                {_.map(searchResult, getDaySearchResult)}
+                {_.map(foundResult, getDaySearchResultDisplay)}
+                {dateStringCursor && <Button title='Load more results' onPress={() => performSearch()} />}
             </ScrollView>
             <Toast />
         </SafeAreaView>
